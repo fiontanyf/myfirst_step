9e731b90037815d4a4b36b1c1e7cac4d73555161
fiontanyf <fiontanyf@gmail.com>
2018-03-21 20:40:46 +0000

    my first app

--- hello.py
+++ hello.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("MyApp")
 
 #import random
-@app.route("/idoexist")
-def idoexist():
-    return "I do exist now!"
+@app.route("/")
+def hello():
+    return "hello world!"
 
+@app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
 
-@app.route("/fionnewpage")
-def mypage():
-    return "this is my new page"
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data = request.form
+    print form_data["email"]
+    return "All OK"
+
 
 app.run(debug=True)
